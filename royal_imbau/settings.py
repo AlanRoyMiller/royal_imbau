b5371d5baaa216d78ebd0a85c5103ce81def2984
Roy <armiller1612@gmail.com>
2025-05-26 20:16:34 +0200

    check static

--- royal_imbau/settings.py
+++ royal_imbau/settings.py
@@ -151,7 +151,7 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR/"production_static"
 
 # Default primary key field type
