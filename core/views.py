63679d11da9b9c2f4e7f6345234bed1cc7174688
Roy Miller <67011496+AlanRoyMiller@users.noreply.github.com>
2025-05-26 14:21:41 +0200

    testing form

--- core/views.py
+++ core/views.py
@@ -8,6 +8,50 @@ logger = logging.getLogger(__name__)
 
 
 def home(request):
+    if request.method == 'POST':
+        form = ContactMessageForm(request.POST)
+
+        # check if the form is valid and clean the data
+        if form.is_valid():
+            logger.info("Form is valid")
+
+            # save the form
+            try:
+                instance = form.save()
+
+                # prepare the message
+                full_message = (
+                    f"First Name: {instance.first_name}\n"
+                    f"Last Name: {instance.last_name}\n"
+                    f"Email: {instance.email}\n"
+                    f"Phone: {instance.phone}\n\n"
+                    f"{instance.message}"
+                )
+
+                # send the email
+                email = EmailMessage(
+                    subject=instance.subject,
+                    body=full_message,
+                    from_email='royal.imbau.mail@gmail.com',  # This is your sending Gmail address
+                    to=['alejandro.imbau@gmail.com'],  # Your Gmail address where you want to receive the form details
+                    reply_to=[instance.email],  # This allows you to reply directly to the person who filled out the form
+                )
+                email.send()
+
+                logger.info("Email sent successfully")
+            except Exception as e:
+                logger.error(f"Error during form submission: {e}")
+                return render(request, 'core/home.html', {'form': form, 'submission_error': True})
+
+            return redirect('/home/?submitted=True')
+        else:
+            logger.error(f"Form errors: {form.errors}")  # Log form errors if the form is not valid
+            return render(request, 'core/home.html', {'form': form})
+    else:
+        form = ContactMessageForm()
+
+    form_submitted = 'submitted' in request.GET
+    return render(request, 'core/home.html', {'form': form})
     return render(request, 'core/home.html')
 
 
