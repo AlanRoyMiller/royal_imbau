c10f6d80cf70842b9df42d7aba669788e7ab79ef
Roy <armiller1612@gmail.com>
2025-05-27 17:04:15 +0200

    feat: update ContactMessageForm to include placeholder translations and improve localization

--- core/forms.py
+++ core/forms.py
@@ -1,15 +1,24 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 from .models import ContactMessage
 
 class ContactMessageForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
         fields = ['first_name', 'last_name', 'email', 'phone', 'subject', 'message']
+        labels = {
+            'first_name': _("First Name"),
+            'last_name': _("Last Name"),
+            'email': _("Email"),
+            'phone': _("Phone"),
+            'subject': _("Subject"),
+            'message': _("Message"),
+        }
         widgets = {
-            'first_name': forms.TextInput(attrs={'placeholder': 'First Name'}),
-            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
-            'phone': forms.TextInput(attrs={'placeholder': 'Phone'}),
-            'subject': forms.TextInput(attrs={'placeholder': 'Subject'}),
-            'message': forms.Textarea(attrs={'placeholder': 'Message'}),
+            'first_name': forms.TextInput(attrs={'placeholder': _("First Name")}),
+            'last_name': forms.TextInput(attrs={'placeholder': _("Last Name")}),
+            'email': forms.EmailInput(attrs={'placeholder': _("Email")}),
+            'phone': forms.TextInput(attrs={'placeholder': _("Phone")}),
+            'subject': forms.TextInput(attrs={'placeholder': _("Subject")}),
+            'message': forms.Textarea(attrs={'placeholder': _("Message")}),
         }
